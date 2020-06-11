2214b3a55eb74aacb8a898f5f88e7adf467044ec
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-06-10 18:14:12 -0700

    adding more logic

--- src/com/company/GroceryList.java
+++ src/com/company/GroceryList.java
@@ -1,6 +1,64 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GroceryList {
+
+    //-----------------Fields-----------------//
+
+    private ArrayList<Item> grocery_list;
+
+    //-----------------Methods----------------//
+
+    public GroceryList(){
+        this.grocery_list = new ArrayList<>();
+    }
+
+    public void addItem(Item itm){
+        this.grocery_list.add(itm);
+    }
+
+    //find an item in the list and update it's qty
+    //TODO: ABSTRACT OUT THE QUESTIONS AND LOGIC TO MAIN, AND USE ARRAY LIST METHODS TO SIMPLY FIND THE ITEM AND RETURN IT BACK
+    public void updateItem(String item){
+        if(this.grocery_list.isEmpty()){
+            System.out.println("List is empty");
+            return;
+        }
+        //find this item
+        for(Item itm : grocery_list){
+            if(itm.itemName.equals(item)){
+                System.out.println("Increment or decrement? [+ / -]");
+                Scanner scan = new Scanner(System.in);
+                String choice = scan.nextLine();
+                if(choice.equals("+")){
+                    System.out.printf("How many more %s's would you like to add?",itm.itemName);
+                    scan.nextInt();
+                    itm.QTY +=1;
+                }else{
+                    System.out.printf("How many more %s's would you like to remove?",itm.itemName);
+                    scan.nextInt();
+                    itm.QTY -= 1;
+                }
+            }
+            else{
+                System.out.println("Could not find item");
+            }
+            return;
+        }
+    }
+
+    //Get the current list
+    public void getList(){
+        System.out.println("Grocery List");
+        for(Item i : grocery_list){
+            System.out.printf("Item name: %s\n" +
+                    "Quantity: %d\n" +
+                    "Description: %s\n" +
+                    "\n\n",i.itemName,i.QTY,i.desc);
+        }
+    }
 
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,62 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    //class level scanner field
+    private static final Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+
+
+        //define new instance of grocery list
+        GroceryList groceryList = new GroceryList();
+
+        boolean cont = true;
+        //Menu
+        while(cont){
+            System.out.println("Enter an action:\n" +
+                    "1)Add an item\n" +
+                    "2)Update an item's quantity\n" +
+                    "3)Delete an item\n" +
+                    "4)Get item contents\n" +
+                    "5)Clear grocery list\n" +
+                    "6)See current list\n" +
+                    "7)Quit and see summary");
+            switch (scan.nextInt()) {
+                //enhanced for syntax using lambda ->
+                case 1 -> {
+                    Item itm = getItem();
+                    groceryList.addItem(itm);
+                }
+                case 2 -> {
+                    System.out.println("What item do you wish to update?");
+                    scan.nextLine();
+                    String item = scan.nextLine();
+                    groceryList.updateItem(item);
+                }
+                case 6 ->{
+                    groceryList.getList();
+                }
+                case 7 -> cont = false;
+                default -> System.out.println("Please only enter 1-7");
+            }
+        }
+        System.out.println("Thank you");
+
+    }
+
+    public static Item getItem(){
+        Item itm = new Item();
+        scan.nextLine();
+        System.out.println("What item are you adding to the list?");
+        itm.itemName = scan.nextLine();
+        System.out.printf("What is the quantity of %s's you are adding?\n",itm.itemName);
+        itm.QTY = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Please provide a description: ");
+        itm.desc = scan.nextLine();
+        return itm;
     }
 }
